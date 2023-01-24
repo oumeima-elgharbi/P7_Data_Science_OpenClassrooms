6544625827288e94cd48f137ffcaec4031cd0248
Oumeima El Gharbi <oumeima.elgharbi@gmail.com>
2023-01-24 12:06:32 +0100

    starting clean dashboard

--- dashboard.py
+++ dashboard.py
@@ -2,61 +2,86 @@ import pandas as pd
 import streamlit as st
 import requests
 
+# 1) Endpoints declaration
+
 HOST = 'http://127.0.0.1:8000'
 
-client_id = st.sidebar.number_input('Insert client id',
-                                    value=100001)
+endpoint_get_client_data = "/clients/{}/"
+endpoint_predict = "/predict/"
+endpoint_shap = "/shap/"
+
+client_id = st.sidebar.number_input('Insert client id', value=100001)  # default value 100001 # to change later to 0
 
 
-# payload = {
-#    "client_id": str(client_id)
-# }
+# 2) GET client / POST predict / POST shap
 
-def request_prediction(model_uri, client_id):
-    # headers = {"Content-Type": "application/json"}
-
-    # data_json = {'data': data}
-    response = requests.request(
-        method='POST', url=model_uri.format(client_id))  # headers=headers, url=model_uri)#, json=data_json)
-
-    if response.status_code != 200:
-        raise Exception(
-            "Request failed with status {}, {}".format(response.status_code, response.text))
-
-    return response.json()
-
-######################
 def request_client_data(model_uri, client_id):
-    # headers = {"Content-Type": "application/json"}
+    """
 
-    # data_json = {'data': data}
+    :param model_uri:
+    :param client_id: (int)
+    :return:
+    """
     response = requests.request(
-        method='GET', url=model_uri.format(client_id))  # headers=headers, url=model_uri)#, json=data_json)
+        method='GET', url=model_uri.format(client_id))
 
     if response.status_code != 200:
         raise Exception(
             "Request failed with status {}, {}".format(response.status_code, response.text))
 
     return response.json()
-##########################
+
+
+def request_prediction(model_uri, client_json):
+    """
+
+    :param model_uri:
+    :param client_json:
+    :return:
+    """
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request(
+        method='POST', headers=headers, url=model_uri, json=client_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
+
+
+def request_shap(model_uri, client_json):
+    """
+
+    :param model_uri:
+    :param client_json:
+    :return:
+    """
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request(
+        method='POST', headers=headers, url=model_uri, json=client_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
+
+
+# 3) dashboard front-end
 
 def main():
-    FASTAPI_URI = HOST + '/clients/{}/predict/'
-
-    api_choice = st.sidebar.selectbox(
-        'Quelle API souhaitez vous utiliser',
-        ['FastAPI'])
-
     st.title('Client n°{} application for a loan'.format(client_id))
-
-    revenu_med = st.number_input('Revenu médian dans le secteur (en 10K de dollars)',
-                                 min_value=0., value=3.87, step=1.)
 
     predict_btn = st.button('Prédire')
     if predict_btn:
-        #pred = None
-        #if api_choice == 'FastAPI':
-        pred = request_prediction(FASTAPI_URI, client_id)
+        pred = None  # ??
+
+        # we get the json body for the client_id selected
+        client_json = request_client_data(HOST + endpoint_get_client_data, client_id)
+        pred = request_prediction(HOST + endpoint_predict, client_json) # we get the prediction
 
         st.write('Probability that the client pays back the loan {} %'.format(round(100 * pred, 2)))
 
@@ -64,10 +89,3 @@ def main():
 if __name__ == '__main__':
     main()
     # streamlit run dashboard.py
-
-
-## I need the data from the client in the body ???
-## {client_data: [], probability: []}
-
-## GET get_client_data ??
-## POST prediction
