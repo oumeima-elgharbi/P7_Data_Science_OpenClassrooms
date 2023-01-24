3f45b7037cf16c0bd09df42288605c8d5e0fd589
Oumeima El Gharbi <oumeima.elgharbi@gmail.com>
2023-01-24 11:34:48 +0100

    endpoints OK ? get_client / predict / SHAP

--- FastAPI_app/main.py
+++ FastAPI_app/main.py
@@ -24,18 +24,14 @@ print("__Loading classifier")
 model = load_model(config["classifier"])
 
 
-# @app.get('/clients/{client_id}')
-# def base(client_id: int):
-#    return f"Client id is {client_id} & it's type is : {type(client_id)}"
-
 @app.get('/clients/{client_id}')
 def get_client_data(client_id: int):
     """
     Body empty, using the client's id, we get the client's preprocessed data
-    
-    :param client_id:
-    :param real_time: if False, we return the preprocessed client's application
-    :return:
+
+    :param client_id: (int)
+    :return: a preprocessed client with feature / value
+    :rtype: (dict)
     """
     print("__Getting client's application data from database__")
     client_df = preprocess_one_application(client_id)
@@ -53,7 +49,8 @@ async def predict(client_json: dict = Body({})):  # remove async def ?? # :dict 
     Returns the probability that the client will repay the loan
 
     :param client_json: (dict) json formatted preprocessed client
-    :return:
+    :return: the probability that the client will repay the loan
+    :rtype: (float)
     """
     print("_____Start of prediction pipeline_____")
     print("_____Getting client_____")
@@ -62,12 +59,42 @@ async def predict(client_json: dict = Body({})):  # remove async def ?? # :dict 
 
     print("_____Predicting_____")
     probability = get_prediction_proba(model, client_df)
-    # print(probability)
-    # print(probability[:, 1])
-    # proba = round(probability[:, 1][0] * 100, 2)
-    # response = [{'client ID': client_id}, {'probability of paying back the loan in %': proba}]
-    response = probability[:, 1][0]
-    return response  # return prediction / [0] to get the value
+    response = probability[:, 1][0]  # return prediction / [0] to get the value
+    return response
+
+
+@app.post('/shap/')
+def get_shap(client_json: dict = Body({})):
+    """
+    Computes SHAP values for each feature for a client
+    ##the probability of default for a client.
+
+    :param client_json: (dict) json formatted preprocessed client
+    :return: List of dict, each dict is a feature + its SHAP value
+    :rtype: (list)
+    """
+    print("_____Start of SHAP_____")
+
+    print("_____Getting client_____")
+    client_df = pd.DataFrame.from_dict(client_json, orient="index").transpose()
+    # client_df = pd.Series(client_json).to_frame().transpose()
+
+    print("_____Getting SHAP for our client_____")
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(client_df)
+    df_shap = pd.DataFrame({
+        'SHAP value': shap_values[1][0],
+        'feature': client_df.columns
+    })
+    df_shap.sort_values(by='SHAP value', inplace=True, ascending=False)
+
+    # transforming df to json response
+    client_shap_string = df_shap.to_json(orient='records')
+    client_shap_json = json.loads(client_shap_string)  # list of dict
+
+    print("We can verify that we have as much SHAP values as we have features for our client : ", len(client_shap_json),
+          len(client_shap_json) == client_df.shape[1])
+    return client_shap_json
 
 
 # analyse sur le client (stat) : sur les 3 var les plus importantes // comaprer avec la moy des clients refusés et acceptes /
@@ -79,24 +106,3 @@ async def predict(client_json: dict = Body({})):  # remove async def ?? # :dict 
 # sur DB : ajouter info sur client / genre - salaire - etc
 
 # salaire : si modifie / personne acceptée ou pas (bonus)
-
-#######################
-@app.get('/shap/')
-def get_shap(json_client: dict = Body({})):
-    """
-    Calculates the probability of default for a client.
-    Args:
-    - client data (json).
-    Returns:
-    - SHAP values (json).
-    """
-    df_one_client = pd.Series(json_client).to_frame().transpose()
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(df_one_client)
-    df_shap = pd.DataFrame({
-        'SHAP value': shap_values[1][0],
-        'feature': df_one_client.columns
-    })
-    df_shap.sort_values(by='SHAP value', inplace=True, ascending=False)
-    return df_shap.to_json(orient='index')
-#############################""
