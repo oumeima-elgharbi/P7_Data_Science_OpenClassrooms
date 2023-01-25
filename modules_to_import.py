80e52e2de29f35958c7159d27067752024070069
Oumeima El Gharbi <oumeima.elgharbi@gmail.com>
2023-01-25 15:35:00 +0100

    requirements updated before heroku

--- modules_to_import.py
+++ modules_to_import.py
@@ -1,1 +1,13 @@
-import pandas as pd
+import uvicorn
+import streamlit
+
+import warnings
+from time import time, strftime, gmtime
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+import unittest
+import pytest
+
