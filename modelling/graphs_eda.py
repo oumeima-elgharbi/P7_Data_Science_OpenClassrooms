6f5e5917939d59ec90002bbb62fd8e06dd4cb7ca
Oumeima El Gharbi <oumeima.elgharbi@gmail.com>
2023-02-22 09:58:49 +0100

    renamed model_dev folder to modelling

---
+++ modelling/graphs_eda.py
@@ -0,0 +1,294 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+def load_data(path, filename):
+    """
+
+    :param path:
+    :param filename: (string)
+    :return:
+    :rtype: (DataFrame)
+    """
+    print("___Loading raw dataset___")
+
+    # Load raw data
+    dataset_file = "{}{}".format(path, filename)
+    dataset = pd.read_csv(dataset_file, encoding="windows-1252")
+
+    print("Initial shape :", dataset.shape)
+    return dataset
+
+
+def missing_data(data):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param data:
+    :return:
+    :rtype: (DataFrame)
+    """
+    total = data.isnull().sum().sort_values(ascending=False)
+    percent = (data.isnull().sum() / data.isnull().count() * 100).sort_values(ascending=False)
+    return pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+
+def plot_stats(df, feature, label_rotation=False, horizontal_layout=True):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param label_rotation:
+    :param horizontal_layout:
+    :return: None
+    :rtype: None
+    """
+    temp = df[feature].value_counts()
+    df1 = pd.DataFrame({feature: temp.index, 'Number of contracts': temp.values})
+
+    # Calculate the percentage of target=1 per category value
+    cat_perc = df[[feature, 'TARGET']].groupby([feature], as_index=False).mean()
+    cat_perc.sort_values(by='TARGET', ascending=False, inplace=True)
+
+    if (horizontal_layout):
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+    else:
+        fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 14))
+    sns.set_color_codes("pastel")
+    s = sns.barplot(ax=ax1, x=feature, y="Number of contracts", data=df1)
+    if (label_rotation):
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+
+    s = sns.barplot(ax=ax2, x=feature, y='TARGET', order=cat_perc[feature], data=cat_perc)
+    if (label_rotation):
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+    plt.ylabel('Percent of target with value 1 [%]', fontsize=10)
+    plt.tick_params(axis='both', which='major', labelsize=10)
+
+    plt.show();
+
+
+def plot_distribution(df, feature, color):
+    """
+    # Plot distribution of one feature
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param color:
+    :return: None
+    :rtype: None
+    """
+    plt.figure(figsize=(10, 6))
+    plt.title("Distribution of %s" % feature)
+    sns.distplot(df[feature].dropna(), color=color, kde=True, bins=100)
+    plt.show()
+
+
+def plot_distribution_comp(df, var, nrow=2):
+    """
+    # Plot distribution of multiple features, with TARGET = 1/0 on the same graph
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param var:
+    :param nrow:
+    :return: None
+    :rtype: None
+    """
+
+    i = 0
+    t1 = df.loc[df['TARGET'] != 0]
+    t0 = df.loc[df['TARGET'] == 0]
+
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(nrow, 2, figsize=(12, 6 * nrow))
+
+    for feature in var:
+        i += 1
+        plt.subplot(nrow, 2, i)
+        sns.kdeplot(t1[feature], bw=0.5, label="TARGET = 1")
+        sns.kdeplot(t0[feature], bw=0.5, label="TARGET = 0")
+        plt.ylabel('Density plot', fontsize=12)
+        plt.xlabel(feature, fontsize=12)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.show();
+
+
+def plot_bureau_stats(df, feature, label_rotation=False, horizontal_layout=True):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param label_rotation:
+    :param horizontal_layout:
+    :return: None
+    :rtype: None
+    """
+    temp = df[feature].value_counts()
+    df1 = pd.DataFrame({feature: temp.index, 'Number of contracts': temp.values})
+
+    # Calculate the percentage of target=1 per category value
+    cat_perc = df[[feature, 'TARGET']].groupby([feature], as_index=False).mean()
+    cat_perc.sort_values(by='TARGET', ascending=False, inplace=True)
+
+    if horizontal_layout:
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+    else:
+        fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 14))
+    sns.set_color_codes("pastel")
+    s = sns.barplot(ax=ax1, x=feature, y="Number of contracts", data=df1)
+    if label_rotation:
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+
+    s = sns.barplot(ax=ax2, x=feature, y='TARGET', order=cat_perc[feature], data=cat_perc)
+    if label_rotation:
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+    plt.ylabel('Percent of target with value 1 [%]', fontsize=10)
+    plt.tick_params(axis='both', which='major', labelsize=10)
+
+    plt.show();
+
+
+def plot_bureau_distribution(df, feature, color):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param color:
+    :return: None
+    :rtype: None
+    """
+    plt.figure(figsize=(10, 6))
+    plt.title("Distribution of %s" % feature)
+    sns.distplot(df[feature].dropna(), color=color, kde=True, bins=100)
+    plt.show()
+
+
+def is_outlier(points, thresh=3.5):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    # Source: https://stackoverflow.com/questions/11882393/matplotlib-disregard-outliers-when-plotting (see references)
+    Returns a boolean array with True if points are outliers and False
+    otherwise.
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
+    """
+    if len(points.shape) == 1:
+        points = points[:, None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median) ** 2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+
+def plot_bureau_outliers_distribution(df, feature, color):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param color:
+    :return:
+    :rtype:
+    """
+    plt.figure(figsize=(10, 6))
+    plt.title("Distribution of %s" % feature)
+    x = df[feature].dropna()
+    filtered = x[~is_outlier(x)]
+    sns.distplot(filtered, color=color, kde=True, bins=100)
+    plt.show()
+
+
+def plot_bureau_distribution_comp(df, var, nrow=2):
+    """
+    # Plot distribution of multiple features, with TARGET = 1/0 on the same graph
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param var:
+    :param nrow:
+    :return: None
+    :rtype: None
+    """
+    i = 0
+    t1 = df.loc[df['TARGET'] != 0]
+    t0 = df.loc[df['TARGET'] == 0]
+
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(nrow, 2, figsize=(12, 6 * nrow))
+
+    for feature in var:
+        i += 1
+        plt.subplot(nrow, 2, i)
+        sns.kdeplot(t1[feature], bw=0.5, label="TARGET = 1")
+        sns.kdeplot(t0[feature], bw=0.5, label="TARGET = 0")
+        plt.ylabel('Density plot', fontsize=12)
+        plt.xlabel(feature, fontsize=12)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.show();
+
+
+def plot_previous_application_stats(df, feature, label_rotation=False, horizontal_layout=True):
+    """
+    Source : https://www.kaggle.com/code/gpreda/home-credit-default-risk-extensive-eda#Explore-the-data
+
+    :param df:
+    :param feature:
+    :param label_rotation:
+    :param horizontal_layout:
+    :return:
+    :rtype:
+    """
+    temp = df[feature].value_counts()
+    df1 = pd.DataFrame({feature: temp.index, 'Number of contracts': temp.values})
+
+    # Calculate the percentage of target=1 per category value
+    cat_perc = df[[feature, 'TARGET']].groupby([feature], as_index=False).mean()
+    cat_perc.sort_values(by='TARGET', ascending=False, inplace=True)
+
+    if horizontal_layout:
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+    else:
+        fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 14))
+    sns.set_color_codes("pastel")
+    s = sns.barplot(ax=ax1, x=feature, y="Number of contracts", data=df1)
+    if label_rotation:
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+
+    s = sns.barplot(ax=ax2, x=feature, y='TARGET', order=cat_perc[feature], data=cat_perc)
+    if label_rotation:
+        s.set_xticklabels(s.get_xticklabels(), rotation=90)
+    plt.ylabel('Percent of target with value 1 [%]', fontsize=10)
+    plt.tick_params(axis='both', which='major', labelsize=10)
+
+    plt.show();
---
+++ modelling/train_models.py
@@ -0,0 +1,169 @@
+from lightgbm import LGBMClassifier
+import re
+import numpy as np
+import pandas as pd
+import gc
+import time
+
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold, StratifiedKFold
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+from contextlib import contextmanager
+import os
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Create local directories to save data
+print("__Creating folders locally__")
+os.makedirs("dataset/cleaned/LGBM", exist_ok=True)
+os.makedirs("models/LGBM", exist_ok=True)
+
+global seed
+seed = 1001
+
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
+
+# LightGBM GBDT with KFold or Stratified KFold
+# Parameters from Tilii kernel: https://www.kaggle.com/tilii7/olivier-lightgbm-parameters-by-bayesian-opt/code
+def kfold_lightgbm(df, num_folds, stratified=False):
+    """
+
+    :param df:
+    :param num_folds:
+    :param stratified:
+    :return:
+    :rtype:
+    """
+    # Divide in training/validation set
+    train_df = df.copy()
+
+    print("Starting LightGBM. Train shape: {}".format(train_df.shape))
+    del df
+    gc.collect()
+    # Cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
+    else:
+        folds = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
+    # Create arrays and dataframes to store results
+    oof_preds = np.zeros(train_df.shape[0])
+    # sub_preds = np.zeros(test_df.shape[0])
+    feature_importance_df = pd.DataFrame() # put SK_ID_CURR in pd.read_csv so not needed here below
+    feats = [f for f in train_df.columns if f not in ['TARGET', 'SK_ID_CURR']] # 'SK_ID_BUREAU', 'SK_ID_PREV', 'index' # deleted
+
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
+        print("Iteration : ", n_fold)
+        # train_idx = index of rows from the training set
+        # valid_idx = index of rows from the validation set
+        train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
+        valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
+
+        # LightGBM parameters found by Bayesian optimization
+        clf = LGBMClassifier(
+            nthread=4,
+            n_estimators=10000,
+            learning_rate=0.02,
+            num_leaves=34,
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            max_depth=8,
+            reg_alpha=0.041545473,
+            reg_lambda=0.0735294,
+            min_split_gain=0.0222415,
+            min_child_weight=39.3259775,
+            silent=-1,
+            verbose=-1, )
+
+        train_x = train_x.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x))
+        valid_x = valid_x.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x))
+        # train_y = train_y.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+
+        print("Saving X_train, X_val, y_train and y_val")
+        train_x.to_csv("dataset/cleaned/LGBM/X_train_fold_{}.csv".format(n_fold), index=False)
+        valid_x.to_csv("dataset/cleaned/LGBM/X_val_fold_{}.csv".format(n_fold), index=False)
+
+        # print("HERE", train_y.shape, valid_y.shape)
+        train_y_df = pd.DataFrame(data=train_y, columns=["target"], index=train_x.index)
+        valid_y_df = pd.DataFrame(data=valid_y, columns=["target"], index=valid_x.index)
+
+        train_y_df.to_csv("dataset/cleaned/LGBM/y_train_fold_{}.csv".format(n_fold), index=False)
+        valid_y_df.to_csv("dataset/cleaned/LGBM/y_val_fold_{}.csv".format(n_fold), index=False)
+
+        print("Training LGBM")
+        print("Training shape :", train_x.shape, "Validation shape :", valid_x.shape)
+        clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)],
+                eval_metric='auc', verbose=200, early_stopping_rounds=200)
+
+        # save model
+        # print("Saving LGBM")
+        # filename_txt = 'models/LGBM/LGBMClassifier_fold_{}.txt'.format(n_fold)
+        # clf.booster_.save_model(filename_txt)
+
+        # save the model to disk
+        print("Saving LGBM with joblib")
+        filename_joblib = 'models/LGBM/LGBMClassifier_fold_{}.joblib'.format(n_fold)
+        joblib.dump(clf, filename_joblib)
+
+        # load model
+        print("Loading LGBM")
+        clf = joblib.load(filename_joblib)
+        # clf = lgb.Booster(model_file='models/LGBMClassifier_fold_{}.txt'.format(i))
+        # with Booster use predict : returns the proba
+
+        print(
+            "Predicting proba for class 1")  # [:, 1] to get the column for class 1 proba, else saving two columns => problem
+        # valid_idx = validation set index
+        # saving the prediction proba of validation set into oof_preds np array
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats
+        fold_importance_df["importance"] = clf.feature_importances_
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+        print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
+        del clf, train_x, train_y, valid_x, valid_y
+        gc.collect()
+
+    print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
+    # Plot feature importance
+    display_importances(feature_importance_df)
+    return feature_importance_df
+
+
+# Display/plot feature importance
+def display_importances(feature_importance_df_):
+    cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(by="importance",
+                                                                                                   ascending=False)[
+           :40].index
+    best_features = feature_importance_df_.loc[feature_importance_df_.feature.isin(cols)]
+    plt.figure(figsize=(8, 10))
+    sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig('models/LGBM/lgbm_importances01.png')
+
+
+def modelling_lightgbm(df_path, debug=False):
+    print("__Loading DataFrame__")
+    df = pd.read_csv(df_path, index_col="SK_ID_CURR")
+    with timer("Run LightGBM with kfold"):
+        feat_importance = kfold_lightgbm(df, num_folds=10, stratified=False)
+
+
+
+
+
+if __name__ == "__main__":
+    with timer("Full model run"):
+        print("Running time 3h")
+        modelling_lightgbm(df_path="dataset/cleaned/train.csv") # data_train_preprocessed_vf
